1bce55b09a3e8115728571f1df9c588c0c7f366b
Valenjess <jvalentina.gaviria@udea.edu.co>
2021-10-11 22:41:55 -0500

    Creando funciones para las ecuaciones fisicas

--- universe.cpp
+++ universe.cpp
@@ -12,11 +12,14 @@ Universe::Universe(QWidget *parent)
     lectura();
 
     QPen pen1(Qt::yellow, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    QPen pen2(Qt::green, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
 
     l.push_back(scene->addEllipse(Posiciones[0],Posiciones[1],Radios[0],Radios[0],pen1));
+    l.push_back(scene->addEllipse(Posiciones[2],Posiciones[3],Radios[1],Radios[1],pen2));
+    l.push_back(scene->addEllipse(Posiciones[4],Posiciones[5],Radios[2],Radios[2],pen2));
 
     ui->graphicsView->setScene(scene);
-    ui->graphicsView->showFullScreen();
+    ui->graphicsView->showMinimized();
 
 }
 
@@ -83,6 +86,46 @@ void Universe::lectura()
 
 }
 
+
+float Cal_Distance(float Xpla,float Ypla, float XSun, float YSun )
+{
+    float Distance = 0;
+
+    Distance = sqrt(pow((XSun-Xpla),2)+pow((YSun-Ypla),2));
+
+    return Distance;
+
+}
+
+float Angulo(float Xpla,float Ypla, float XSun, float YSun){
+
+    float angulo = 0;
+    angulo = atan((YSun-Ypla)/(XSun-Xpla))*57,2958;
+
+    return angulo;
+}
+
+void AceleraX(float Distance, float angulo){
+
+    float AcelX = 0;
+    float Gravity = 1;
+    float MasaSun = 70000;
+
+    AcelX = ((Gravity*MasaSun)/(pow(Distance,2)))*cos(angulo);
+    cout<<AcelX;
+
+}
+
+void AceleraY(float Distance, float angulo){
+
+    float AcelX = 0;
+    float Gravity = 1;
+    float MasaSun = 70000;
+
+    AcelX = ((Gravity*MasaSun)/(pow(Distance,2)))*cos(angulo);
+    cout<<AcelX;
+
+}
 
 Universe::~Universe()
 {
